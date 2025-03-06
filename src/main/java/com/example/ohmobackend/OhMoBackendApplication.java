b75e00c1c5ea78bf83a525a8f941a20be4d83ea5
hyojin425 <gywls5184@gmail.com>
2025-03-06 16:37:12 +0900

    feat: Initial Commit

---
+++ src/main/java/com/example/ohmobackend/OhMoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohmobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhMoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ohmobackend/OhMoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohmobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
