26af98534d1095f90c7787720088469de87c92c9
hyojin425 <gywls5184@gmail.com>
2025-03-26 17:58:19 +0900

    refactor: 일별 일정 service member와 날짜로 일정 조회

--- src/main/java/com/example/ohmobackend/repository/ScheduleRepository.java
+++ src/main/java/com/example/ohmobackend/repository/ScheduleRepository.java
@@ -1,18 +1,32 @@
 package com.example.ohmobackend.repository;
 
+import com.example.ohmobackend.domain.Member;
 import com.example.ohmobackend.domain.MemberCategory;
 import com.example.ohmobackend.domain.Schedule;
+import com.example.ohmobackend.domain.enums.ScheduleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    @Query("SELECT s FROM Schedule s WHERE s.memberCategory = :memberCategory AND s.date = :date")
-    public List<Schedule> findByMemberCategoryAndDate(MemberCategory memberCategory, LocalDate date);
+    @Query("SELECT s FROM Schedule s WHERE s.member = :member AND s.date = :date AND s.scheduleType = :scheduleType")
+    public List<Schedule> findByMemberAndDateAndScheduleType(
+            @Param("member") Member member,
+            @Param("date") LocalDate date,
+            @Param("scheduleType") ScheduleType scheduleType
+    );
 
     @Query("SELECT s FROM Schedule s WHERE s.memberCategory = :memberCategory AND FUNCTION('DATE_FORMAT', s.date, '%Y-%m') = :month")
     public List<Schedule> findByMemberCategoryAndMonth(MemberCategory memberCategory, String month);
+
+    @Query("SELECT s FROM Schedule s WHERE s.member = :member AND s.date = :date AND s.scheduleType = :scheduleType AND s.status = true")
+    public List<Schedule> findByMemberAndDateAndScheduleTypeAndStatusIsTrue(
+            @Param("member") Member member,
+            @Param("date") LocalDate date,
+            @Param("scheduleType") ScheduleType scheduleType
+    );
 }
