fbe0847e88857444c22f2e7ca2d36ea3fc0e7a01
hyojin425 <gywls5184@gmail.com>
2025-10-03 02:04:55 +0900

    fix: 불필요한 코드 제거

--- src/main/java/com/example/ohmobackend/repository/RoutineWeekRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.ohmobackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutineWeekRepository extends JpaRepository<RoutineWeek, Long> {
-}
